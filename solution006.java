54afa91d7b5c98a37dbb15dd67d2f8b75a4fc99d
Amit <amitvarsani121@googlemail.com>
2013-07-12 12:47:19 +0100

    Create solution006.java

---
+++ solution006.java
@@ -0,0 +1,12 @@
+class solution6
+  public static void main(String[] args){
+		int sum1 = 0; //variable for sum of squares
+		int sum2 = 0; //variable for square of the sums
+		for(int i=1; i<=100; i++){
+			sum1 += (int)Math.pow(i,2); //find sum of squares
+			sum2 += i; // find sum of numbers
+		}
+		sum2 = (int)Math.pow(sum2,2); //square of the sums
+		System.out.println(sum2-sum1); //difference between the sum of the squares and the square of the sums
+	}
+}
