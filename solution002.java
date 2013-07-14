d209cbe242b0a2bb1206059f9c401a79943f43a3
Amit <amitvarsani121@googlemail.com>
2013-07-14 12:52:04 +0100

    Update solution002.java

--- solution002.java
+++ solution002.java
@@ -3,7 +3,7 @@ class solution2{
 		int prev1 =0;
 		int prev2 = 1;
 		int sum = 0;
-		for(int i = 0; prev1<4000000 && prev2<4000000; i++){
+		for(int i = 0; prev1<4000000 && prev2<4000000; i++){ //iteration used to find values of fibonacci sequence
 			int save = prev1;
 			prev1 = prev2;
 			prev2 = save + prev2;
